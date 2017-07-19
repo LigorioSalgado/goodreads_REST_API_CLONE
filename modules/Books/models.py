3b64e7b5349d18cd2da9b4c16c9a360a5914d91c
Edwin Ligorio Salgado <lesf@miflink.com>
2017-07-18 19:25:54 -0500

    first commit

---
+++ modules/Books/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from modules.Authors.models import Author
+#from modules.Users.models import User
+from django.contrib.postgres.fields import ArrayField
+
+
+LIT_GENRE = (
+    ("sf","science fiction"),
+    ("dr","drama"),
+    ("ft","fantasy"),
+    ("bg","biography"),
+    ("ht","history"),
+    ("ot","others"),
+
+)
+
+
+
+class Book(models.Model):
+    id =  models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    ISBN = models.CharField(max_length=18,unique=True)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    date_published = models.DateField()
+    cover_photo = models.URLField()
+    summary = models.TextField()
+    rating = models.DecimalField(decimal_places=2,max_digits=3)
+    lit_genre = models.CharField(max_length=50,choices=LIT_GENRE)
+    tags = ArrayField(
+        models.CharField(max_length=50),
+        size=5
+    )
+
+class Comments(models.Model):
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    
+
+    
+
+
+    
+
+
+
+
+
+

cc3e37ca7f770893a0bda533f4a0f9e67f6d8b7c
Edwin Ligorio Salgado <lesf@miflink.com>
2017-07-28 20:17:18 -0500

    database production

--- goodreads_api/settings/production.py
+++ goodreads_api/settings/production.py
@@ -1,7 +1,19 @@
 from .base import *
+import os
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 
-DATABASES = dict()
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'good_db',
+       'USER': 'good_admin',
+       'PASSWORD':'good2017',
+       'HOST':'localhost',
+       'PORT':'5432'
+   }
+}
+
+STATIC_ROOT = os.path.join(os.getcwd(),'static')

29698d99a9baa4ecaa6a81e7ffb6d1df71f27d3c
Edwin Ligorio Salgado <lesf@miflink.com>
2017-07-28 20:17:18 -0500

    database production

--- goodreads_api/settings/production.py
+++ goodreads_api/settings/production.py
@@ -4,4 +4,13 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
-DATABASES = dict()
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'good_db',
+       'USER': 'good_admin',
+       'PASSWORD':'good2017',
+       'HOST':'localhost',
+       'PORT':'5432'
+   }
+}
